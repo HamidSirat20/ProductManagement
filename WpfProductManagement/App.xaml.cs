0c03aaf0fa7857110402c6350a251b4c511c084c
HamidSirat20 <abeshaqzada@gmail.com>
2024-02-13 23:31:02 +0200

    Add project files.

---
+++ WpfProductManagement/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfProductManagement
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
